ad20f99c6460359a8dbbe5419df3fec360b28c5b
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-11-15 12:11:47 -0300

    Adicionando o Django Admin Docs

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('admin/docs/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
