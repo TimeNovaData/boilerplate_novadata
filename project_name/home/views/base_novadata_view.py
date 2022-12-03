9990e86033fe8912cc65df410859e63ed5c7663b
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-12-02 21:10:36 -0300

    BaseNovadataView

--- project_name/home/views/base_novadata_view.py
+++ project_name/home/views/base_novadata_view.py
@@ -2,10 +2,18 @@ from django.views.generic import TemplateView
 
 
 class BaseNovadataView(TemplateView):
+    need_breadcrumb = True
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["breadcrumb"] = self.get_breadcrumb()
         return context
 
     def get_breadcrumb(self):
-        return []
+        if self.need_breadcrumb:
+            name = self.__class__.__name__
+            raise NotImplementedError(
+                f"A classe {name} não implementou os breadcrumbs. Caso não seja necessário, sete a variável need_breadcrumb para False."
+            )
+        else:
+            return []
