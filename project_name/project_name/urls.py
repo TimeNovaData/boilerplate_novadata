0b5a9e40b7342149faf42fbdc0ec73bfcc4e814a
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-12-04 08:57:29 -0300

    Django debug toolbar

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
         name="redoc",
     ),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
