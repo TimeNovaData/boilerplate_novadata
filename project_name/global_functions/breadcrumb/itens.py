083a03de5986fc0c6f004cff627f25db8db27aaa
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-12-28 20:15:48 -0300

    Melhorias no breadcrumb

--- project_name/global_functions/breadcrumb/itens.py
+++ project_name/global_functions/breadcrumb/itens.py
@@ -1,24 +1,7 @@
 # Itens fixos do breadcrumb que podem ser reutilizados
 
-alterar_dados = {
-    "name": "Alterar dados",
-    "slug": "alterar_dados",
+example = {
+    "name": "Example",
+    "slug": "example",
     "url_name": "",
 }
-
-
-detalhes_simples = {
-    "name": "Detalhes",
-    "slug": "detalhes",
-    "url_name": "",
-}
-
-
-def make_detalhes(url_name, url_params=[], get_params={}):
-    return {
-        "name": "Detalhes",
-        "slug": "detalhes",
-        "url_name": url_name,
-        "url_params": url_params,
-        "get_params": get_params,
-    }
---
+++ project_name/global_functions/utils/__init__.py
@@ -0,0 +1,5 @@
+from .camel_to_snake_case import camel_to_snake_case
+
+__all__ = [
+    camel_to_snake_case,
+]
