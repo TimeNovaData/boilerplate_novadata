cd5d1fbe09c1fb24398dd4298c62e280fad70e06
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-01-16 13:32:04 -0300

    Criação da model profile

---
+++ project_name/home/models/profile.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    """
+    A classe Profile serve para armazernar
+    os(as) profiles do sistema.
+
+    Além de fazer as implementações relacionadas
+    a um único objeto do tipo Profile.
+    """
+
+    usuario = models.OneToOneField(
+        User,
+        verbose_name="Usuário",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.usuario}"
+
+    class Meta:
+        app_label = "core"
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
