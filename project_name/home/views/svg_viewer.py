e137be721d4831c3ff3c7a2276da25abfa1d7af2
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-02-03 17:32:42 -0300

    Deixar a app home no padrão de todas as outras

--- project_name/home/views/svg_viewer.py
+++ project_name/home/views/svg_viewer.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from global_functions.redirect import reverse_lazy_plus
-
 
 
 def svg_viewer(request):
-    context = {
+    context = {}
 
-    }
-
-    return render(request, 'svg_viewer/svg_viewer.html', context)
+    return render(
+        request,
+        "svg_viewer/svg_viewer.html",
+        context,
+    )
